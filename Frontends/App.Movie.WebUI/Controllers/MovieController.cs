0d573b8b6899d29831d2d9201e0818983ee3069b
Akif Akkoyun <144811507+Akif-Akkoyun@users.noreply.github.com>
2025-04-25 17:15:19 +0300

    feature/swagger-fix-movie-list-component
    - Swagger index page not opening issue solved.
    - 10 movie data added, with their visuals.
    - List action of MovieController in WebUI changed to Index.
    - View/Movie/List name changed to View/Movie/Index.
    - "@addTagHelper *, App.Movie.WebUI" added to _ViewImports.cshtml file.
    - "<vc:movie-list />" taghelper added instead of "@await Component.InvokeAsync("MovieList")" to Movie/Index.cshtml file.
    - List function added to MovieListViewComponent.
    - IHttpClientFactory integrated to make requests to API.

---
+++ Frontends/App.Movie.WebUI/Controllers/MovieController.cs
@@ -0,0 +1,25 @@
+﻿using App.Movie.Dto.Dtos.MovieDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace App.Movie.WebUI.Controllers
+{
+    public class MovieController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public MovieController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.v1= "Film Listesi";
+            ViewBag.v2 = "Ana Sayfa";
+            ViewBag.v3 = "Tüm Filmler";
+            return View();
+        }
+    }
+}
