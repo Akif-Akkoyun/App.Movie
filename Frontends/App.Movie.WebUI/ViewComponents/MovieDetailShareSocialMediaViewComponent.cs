14891a3f5fbfcc096619963e7a242744ae0c8a9a
Akif Akkoyun <144811507+Akif-Akkoyun@users.noreply.github.com>
2025-04-29 20:43:20 +0300

    Added Movie Detail Page and Related ViewComponents
    - Added a `MovieDetail` method to the `MovieController` to display the details of a single movie and created its corresponding view.
    - Integrated necessary HTML code from the theme into the `MovieDetail` view.
    - Created the following new ViewComponents:
      - MovieDetailShareSocialMediaViewComponent
      - MovieImageAndWatchTrailerViewComponent
      - MovieOverviewViewComponent
      - MovieRateViewComponent
    - Created views for each of the newly added ViewComponents and used them within the `MovieDetail` page.
    - Added a conditional check (`if`) to the layout to prevent a specific section from rendering on the detail page, fixing a layout shifting issue.
    - Set `ViewBag.PageHero = true` in the `MovieDetail` method to hide the hero section on the detail page.

---
+++ Frontends/App.Movie.WebUI/ViewComponents/MovieDetailShareSocialMediaViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace App.Movie.WebUI.ViewComponents
+{
+    public class MovieDetailShareSocialMediaViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
